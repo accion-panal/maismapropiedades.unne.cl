99bc768ac0eaa1e6f2519ffc4b6d778121af1575
Fabian Salas <fabian.salas.astete@gmail.com>
2024-04-26 19:13:47 -0400

    feat: add cod companyId

--- src/constants/consts/company.js
+++ src/constants/consts/company.js
@@ -1,11 +1,11 @@
 export const company = {
   statusId: 1,
-  companyId: 20143,
+  companyId: 20145,
   codigoUsuarioMaestro: 10075,
 };
 
 export const companyForm = {
-  id: 20143,
+  id: 20145,
 };
 
 export const paginationTopLimit = {
